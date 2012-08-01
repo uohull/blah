807cd960a654e8e03bf317eab91d96dd33fd105f
Simon Lamb <s.lamb@hull.ac.uk>
2012-08-01 16:36:13 +0100

    Minor updates

--- app/controllers/holdings_record_controller.rb
+++ app/controllers/holdings_record_controller.rb
@@ -4,6 +4,12 @@ class HoldingsRecordController < ApplicationController
     if bib_no
        holdings_service = HoldingsService.new
        @holdings_records = holdings_service.find_holdings( bib_no )
+       
+       respond_to do |format|        
+        format.html #index.html.erb
+        format.json { render :json => @holdings_records }
+      end
+
     end
   end
 
