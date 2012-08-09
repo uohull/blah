4606c5d3ee6c3f1eda21fda8cf6349eb4e03ee45
Simon Lamb <s.lamb@hull.ac.uk>
2012-08-09 21:28:23 +0100

    Further updates to Blah

--- app/helpers/blah_helper.rb
+++ app/helpers/blah_helper.rb
@@ -13,8 +13,165 @@ module BlahHelper
     end
   end
 
+  def render_online_resources_partial(document)
+
+    #Get the url fields...        
+    journal_url_display = document.get('journal_url_display', :sep => nil)
+    full_text_url = document.get('url_fulltext_display', :sep => nil)
+    
+    #Check online resources exist...
+    unless journal_url_display.nil? && full_text_url.nil?
+      render :partial => 'catalog/online_resources'
+    end
+
+  end
+
   def field_value_separator
     '; '
   end
+
+
+  def display_field(document, solr_fname, label_text='', dd_class=nil, opts={} )
+
+    label = ""
+    display_field = ""
+
+    dd_class = "class=\"blacklight-#{dd_class}\"" if dd_class
+
+    label = if label_text.length > 0 then label_text end
+
+    if document.has? solr_fname
+      field_value = render_index_field_value(:document => document, :field => solr_fname)
+
+      if opts[:display_as_link] then
+        display_value = '<a href="' + field_value + '">' +  opts[:link_text] + '</a>'
+      else
+        display_value = field_value
+      end   
+
+     display_field = <<-EOS
+        <dt class=#{dd_class}>#{label}</dt>
+        <dd class=#{dd_class}>#{display_value}</dd>
+      EOS
+    end
+
+    display_field.html_safe
+
+  end
+
+  def display_field_within_element(document, solr_fname, element='h1', element_class=nil)
+    if document.has? solr_fname
+      content_tag element, render_index_field_value(:document => document, :field => solr_fname), :class => element_class
+    end    
+  end
+
+
+  def render_online_resources( document )
+
+    render_online_resources = ""
+    title_display = render_index_field_value(:document => document, :field => "title_display")
+   
+    #Lets look in all the usual fields...
+    full_text_url = document.get('url_fulltext_display', :sep => nil)
+
+    unless full_text_url.nil?
+      full_text_url.each do |url|
+        render_online_resources << <<-EOS
+        <tr>
+         <td class="table-td-title">Link</td>
+         <td class="table-td-data"><a target="_blank" href="#{url}">#{title_display}</a></td>
+        </tr>
+        EOS
+      end
+     end
+
+    #Lets now check the journal fields...
+    journal_url_display = document.get('journal_url_display', :sep => nil)
+    journal_url_coverage_display = document.get('journal_url_coverage_display', :sep => nil)
+    
+    unless journal_url_display.nil?
+      journal_url_display.each do |url|
+         link_title = ""
+         #Journal coverage corresponds to same place in array...
+         journal_coverage = journal_url_coverage_display[journal_url_display.index(url)]
+         #If it doesn't exist, use the title for link..  
+         link_title = journal_coverage.nil? ? title_display : journal_coverage
+          render_online_resources << <<-EOS
+          <tr>
+           <td class="table-td-title">Link</td>
+           <td class="table-td-data"><a target="_blank" href="#{url}">#{link_title}</a></td>
+          </tr>
+        EOS
+      end
+
+    end
+    render_online_resources.html_safe
+  end
+
+
+  #display e-journal links for _index_e_journal.html.erb page
+  def display_e_journal_links(document)
+
+      display_e_journal_links = ""
+
+      #Lets now check the journal fields...
+      journal_url_display = document.get('journal_url_display', :sep => nil)
+      journal_url_coverage_display = document.get('journal_url_coverage_display', :sep => nil)
+      
+      unless journal_url_display.nil?
+        journal_url_display.each do |url|
+           link_title = ""
+           #Journal coverage corresponds to same place in array...
+           journal_coverage = journal_url_coverage_display[journal_url_display.index(url)]
+           #If it doesn't exist, use the title for link..  
+           link_title = journal_coverage.nil? ? title_display : journal_coverage
+            display_e_journal_links << <<-EOS
+             <dt class="document-link-dd">Online</dt>
+             <dd class="document-link-dd"><a target="_blank" href="#{url}">#{link_title}</a></dd>
+          EOS
+        end
+      end  
+    
+    display_e_journal_links.html_safe 
+  end
+
+  def render_format_icon( document )
+    format = document.get('format', :sep => nil)
+    
+    case format
+    when 'Book', 'E-Book', 'E-Journal'
+      content_tag(:i, '', :class => 'icon-book') 
+    when 'Thesis'
+      content_tag(:i, '', :class => 'icon-file') 
+    when 'Photocopy'
+      content_tag(:i, '', :class => 'icon-inbox')
+    when 'Periodical'
+      content_tag(:i, '', :class => 'icon-bullhorn')
+    when 'Printed Music'
+      content_tag(:i, '', :class => 'icon-music')
+    when 'Playbill'
+      content_tag(:i, '', :class => 'icon-play')
+    when 'Map'
+      content_tag(:i, '', :class => 'icon-globe')
+    when 'CD Audio', 'Cassette'
+      content_tag(:i, '', :class => 'icon-volume-up')
+    when 'DVD'
+      content_tag(:i, '', :class => 'icon-film')
+    when 'Computer file'
+        content_tag(:i, '', :class => 'icon-hdd')
+    when 'Kit'
+        content_tag(:i, '', :class => 'icon-briefcase')
+    when 'Artefact'
+        content_tag(:i, '', :class => 'icon-tag')
+    else
+      content_tag(:i, '', :class => 'icon-book')
+    end
+
+  end 
+
+
+  def render_shelved_icon
+    content_tag(:i, '', :class => 'icon-map-marker') 
+  end 
 
 end
--- app/helpers/holdings_record_helper.rb
+++ app/helpers/holdings_record_helper.rb
@@ -16,4 +16,96 @@ module HoldingsRecordHelper
     end
   end
 
+  def render_holdings_table(holdings_records)
+
+
+    render_holdings_table = ""
+    bjl_items_records = []
+    kdl_items_records = []
+    map_items_records = []
+    wise_items_records = []
+
+   
+   bjl_items_records = holdings_records.select { |record| record.local_location.downcase.include? "bjl" }
+   kdl_items_records = holdings_records.select { |record| record.local_location.downcase.include? "kdl" }
+   wise_items_records = holdings_records.select { |record| record.local_location.downcase.include? "wise" }
+    
+
+    #BJL Ground Floor Reference (R) 	G 1818 C93 P5 	LIB USE ONLY
+    #location_list = {"bjl" => "Brymor Jones Library", "kdl" => "Keith Donaldson Library"}
+
+    # Playing more generic solutions...
+     # availability = {}
+     # location_list.each do |key,value|
+     #   holdings_records.each do |record|
+     #     if record.local_location.downcase.include?  key.downcase
+     #      
+     #      availability.merge!( Hash[ key => [:name => value, :list =>[ holdings_records.index(record) => [record.local_location, record.call_number, record.availability]]]])
+     #     end 
+     #   end
+     # end
+    
+
+     render_holdings_table << holdings_table(bjl_items_records, "Brynmor Jones Library") <<  holdings_table(kdl_items_records, "Keith Donaldson Library") << holdings_table(wise_items_records, "Wise")
+  
+     return render_holdings_table.html_safe
+
+  end
+
+
+  def holdings_table (records, location_title)
+      
+     holdings_table = ""
+
+     if records.length > 0
+       holdings_table <<  <<-EOS
+          <table class="table availability-table">
+	        <thead>
+            <tr>   
+              <td colspan="3"><h5>#{location_title}</h5><h5></h5></td>
+            </tr>
+          </thead>
+          <tbody>
+              <tr>
+                <th>Location</th>
+                <th></th>
+                <th>Status</th>
+              </tr>        
+        EOS
+      
+        records.each do |record|
+        
+          status = ""
+          
+          if record.availability.downcase.include? "available"
+            status = '<span class="label label-success"><i class="icon-ok icon-white"></i>Available</span>'
+          elsif record.availability.downcase.include? "due"
+            status = '<span class="label label-warning">Due:' + record.availability.sub(/DUE/, '') + '</span>'
+          elsif record.availability.downcase.include? "lib use"
+            status = '<span class="label label-important"><i class="icon-remove icon-white"></i>Library use</span>'
+          else
+            status = record.availability
+          end
+
+          holdings_table << <<-EOS
+           <tr>
+              <td class="table-td-title">#{record.local_location.sub(/BJL/, '')}</td>
+              <td class="table-td-title">#{record.call_number}</td>
+              <td class="table-td-data">#{status}</td>
+           </tr>
+          EOS
+        end 
+        holdings_table << <<-EOS
+            </tbody>
+				  </table>
+        EOS
+
+      end
+      return holdings_table
+
+  end
+
+
 end
+
+
