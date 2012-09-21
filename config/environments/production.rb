08e48d3a44048fd270e79083a018f98dae323d5e
Simon Lamb <s.lamb@hull.ac.uk>
2012-09-21 10:58:06 +0100

    Fixed some assets issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Blah::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = false 
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -60,6 +60,9 @@ Blah::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  #Add the following blah specific js/cs - add2home functionality
+  config.assets.precompile += ['add2home.js', 'add2home.css']
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
