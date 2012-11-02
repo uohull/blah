be5a494cef0fddd0714693503f267ad4b0eab466
Simon Lamb <s.lamb@hull.ac.uk>
2012-11-02 10:14:48 +0000

    F100218449: Add opac notice message for referrers from library.hull.ac.uk

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,10 +7,20 @@ class HomeController < ApplicationController
   
   	# Show the Home page for Bl@H aplication 
   	def show
+
+  		referer = request.referer
+
+ 			#If the referer is library.hull.ac.uk display warning about catalogue terminal issue (images)
+  		unless referer.nil? 
+  			 if referer.include? "library.hull.ac.uk"
+					flash[:notice] = t('blah.home.notice.opac_warning').html_safe
+  			 end
+  		end
+  	
   		#show action uses the blacklight-home layout
   		render :layout => 'blacklight-home'
   	end
-
+  	
   	# Get the FAQ page
 	def faq
 	end
