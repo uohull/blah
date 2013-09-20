9398f8c734614fc73928984f4d6e304ef8f251ec
Simon Lamb <s.lamb@hull.ac.uk>
2013-09-20 15:04:52 +0100

    Removed referral message from frontpage, this is no longer required.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,32 +1,20 @@
 class HomeController < ApplicationController
   include BlacklightGoogleAnalytics::ControllerExtraHead
-	include Blacklight::Configurable
-	include Blacklight::SolrHelper
+  include Blacklight::Configurable
+  include Blacklight::SolrHelper
 
-	copy_blacklight_config_from(CatalogController)
+  copy_blacklight_config_from(CatalogController)
   
-  	# Show the Home page for Bl@H aplication 
-  	def show
+  # Show the Home page for Bl@H aplication 
+  def show
+  end
 
-  		referer = request.referer
+  # Get the FAQ page
+  def faq
+  end
 
- 			#If the referer is library.hull.ac.uk display warning about catalogue terminal issue (images)
-  		unless referer.nil? 
-  			 if referer.include? "library.hull.ac.uk"
-					flash[:notice] = t('blah.home.notice.opac_warning').html_safe
-  			 end
-  		end
-  	
-  		#show action uses the blacklight-home layout
-  		#render :layout => 'blacklight-home'
-  	end
-  	
-  	# Get the FAQ page
-	def faq
-	end
-
-	# Get the cookies page
-	def cookies
-	end
+  # Get the cookies page
+  def cookies
+  end
 
 end
