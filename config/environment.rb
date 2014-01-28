94afd1b492f1fc97485e1937fc955684da76eefb
Simon Lamb <s.lamb@hull.ac.uk>
2014-01-28 12:56:09 +0000

    BLAC-83 Loading application configuration before the application initializes - fixes order issue

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Load the Blah application config - Important needs to be loaded before the Application initializes
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/blah_config.yml")[Rails.env]
+
 # Initialize the rails application
 Blah::Application.initialize!
-
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/blah_config.yml")[Rails.env]
