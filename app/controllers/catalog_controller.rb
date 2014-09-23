e146a4e8cb6f88307b491ad79f48db2fcab27330
Simon Lamb <s.lamb@hull.ac.uk>
2014-09-23 10:03:28 +0100

    BLAC-128 - Title Sort ascending and descending added to results page.  Fix typo in catalog view.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -176,7 +176,8 @@ class CatalogController < ApplicationController
     config.add_sort_field 'score desc, pub_date_sort desc, title_sort asc', :label => 'relevance'
     config.add_sort_field 'pub_date_sort desc, title_sort asc', :label => 'year'
     config.add_sort_field 'author_sort asc, title_sort asc', :label => 'author'
-    config.add_sort_field 'title_sort asc, pub_date_sort desc', :label => 'title'
+    config.add_sort_field 'title_sort asc, pub_date_sort desc', :label => 'title (ascending)'
+    config.add_sort_field 'title_sort desc, pub_date_sort desc', :label => 'title (descending)'
 
     # If there are more than this many search results, no spelling ("did you 
     # mean") suggestion is offered.
